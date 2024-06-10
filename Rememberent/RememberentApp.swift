e92e4a8ec76364af825f6f6a011b95a5417e7653
Alexei Egorov <fraine21@gmail.com>
2024-06-10 13:13:41 +0200

    Project init, add .gitignore

---
+++ Rememberent/RememberentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberentApp.swift
+//  Rememberent
+//
+//  Created by Alexei Egorov on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RememberentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
